c0b001df37c12f6c05ed97981e513b371ce55b2c
974794055 <974794055@qq.com>
2018-09-07 17:40:32 +0800

    first

---
+++ CGXPickerViewFramework/CGXPickerViewFramework/CGXPickerViewFramework.h
@@ -0,0 +1,19 @@
+//
+//  CGXPickerViewFramework.h
+//  CGXPickerViewFramework
+//
+//  Created by 曹贵鑫 on 2018/9/7.
+//  Copyright © 2018年 曹贵鑫. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CGXPickerViewFramework.
+FOUNDATION_EXPORT double CGXPickerViewFrameworkVersionNumber;
+
+//! Project version string for CGXPickerViewFramework.
+FOUNDATION_EXPORT const unsigned char CGXPickerViewFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CGXPickerViewFramework/PublicHeader.h>
+
+
